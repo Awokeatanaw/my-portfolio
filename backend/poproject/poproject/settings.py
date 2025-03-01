c8b15e230f34dc7fca3690e91355380e1ca4b77b
Awokeatanaw <Awokeatanaw12@gmail.com>
2025-03-01 08:44:05 -0900

    add netlify domain

--- backend/poproject/poproject/settings.py
+++ backend/poproject/poproject/settings.py
@@ -93,5 +93,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS Settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://awoke-portfolio.netlify.app",
     "http://localhost:3000",
 ]
