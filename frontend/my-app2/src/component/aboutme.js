261ea687dfe228482c51cf03d6e04b8296bdd944
Awokeatanaw <Awokeatanaw12@gmail.com>
2025-04-13 22:10:24 -0900

    photo location changed

--- frontend/my-app2/src/component/aboutme.js
+++ frontend/my-app2/src/component/aboutme.js
@@ -4,7 +4,7 @@ import myImage from "../image/image1.jpg"; // Import your image
   return (
     <div className='ping-text bg-lightGray w-full md:flex md:justify-between md:space-x-4 p-4'>
       <p className='c absolute md:text-[250px] text-[150px] text-white ml-1 mt-0 z-[-2]'>ABOUTME</p>
-      <div className='md:w-[70%]'>
+      <div className='md:w-[64%]'>
       
       <p className='p1 text-4xl mt-16 md:ml-20 font-bold ml-8'>About me</p>
       
@@ -14,17 +14,17 @@ import myImage from "../image/image1.jpg"; // Import your image
       <p className=' md:ml-20 ml-8'>oriented programming through hands-on projects. My true passion lies in web</p>
       <p className='md:ml-20 ml-8'>development, especially with modern frameworks. I specialize in using React and </p>
       <p className=' md:ml-20 ml-8'>TailwindCSS for the frontend, along with Django for the backend. Proficient in HTML, CSS,</p>
-      <p className=' md:ml-20 ml-8 pt-6'>JavaScript, and Python, I enjoy crafting user-friendly and efficient web applications. I thrive </p>
+      <p className=' md:ml-20 ml-8'>JavaScript, and Python, I enjoy crafting user-friendly and efficient web applications. I thrive </p>
       <p className=' md:ml-20 ml-8'>on creating impactful solutions that enhance user experiences.</p>
       <p className='md:ml-20 ml-8'></p>
       
       </div>
-      <div className='md:w-[30%] mt-5 md:mt-52'>
+      <div className='md:w-[36%] mt-5 md:mt-10'>
         <div className='flex justify-center items-center bg-white shadow-lg border-4'>
       <img
           src={myImage}
           alt="Description"
-          className="w-[380px] h-[400px] object-cover rounded-full shadow-lg"
+          className="w-[350px] h-[400px] object-cover rounded-full shadow-lg"
         />
         </div>
       </div>
